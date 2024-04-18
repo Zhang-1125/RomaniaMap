0b4f0df682c4f02e62ba3817e1a83ba3e2415d96
MoQing <moqing1125@gmail.com>
2024-04-18 15:54:24 +0800

    修复了路径问题
    2024年4月18日15:54:22

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <limits>
 #include "headers/Search.h"
 #include "headers/Run.h"
 
 int main() {
-    Search search("Zerind","Bucharest",R"(C:\Users\MoQing\Desktop\RomaniaMap\Romania.txt)");
+    Search search("Zerind","Bucharest","Romania.txt");
     Run run(&search);
     while (true){
         run.showMenu();
